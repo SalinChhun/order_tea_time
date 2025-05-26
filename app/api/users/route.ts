59700f0eb8e1801792e700b889e17c0da51e1ff1
SalinChhun <salinchhun123079@gmail.com>
2025-05-26 13:19:13 +0700

    first init

---
+++ app/api/users/route.ts
@@ -0,0 +1,83 @@
+import {prisma} from "@/lib/prisma";
+
+
+export async function POST(request: Request) {
+
+    try {
+        const {email, name} = await request.json()
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                name,
+            },
+        })
+
+        return new Response(JSON.stringify(user), {
+            status: 201,
+            headers: {'Content-Type': 'application/json'}
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return Response.json({error: error.message}, {status: 500})
+        }
+    }
+
+}
+
+export async function GET(request: Request) {
+    try {
+        const users = await prisma.user.findMany()
+
+        return new Response(JSON.stringify(users), {
+            status: 200,
+            headers: {'Content-Type': 'application/json'}
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return Response.json({error: error.message}, {status: 500})
+        }
+    }
+}
+
+export async function PUT(request: Request) {
+    try {
+        const {id, email, name} = await request.json()
+
+        const user = await prisma.user.update({
+            where: {id},
+            data: {
+                email,
+                name,
+            },
+        })
+
+        return new Response(JSON.stringify(user), {
+            status: 200,
+            headers: {'Content-Type': 'application/json'}
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return Response.json({error: error.message}, {status: 500})
+        }
+    }
+}
+
+export async function DELETE(request: Request) {
+    try {
+        const {id} = await request.json()
+
+        const user = await prisma.user.delete({
+            where: {id},
+        })
+
+        return new Response(JSON.stringify(user), {
+            status: 200,
+            headers: {'Content-Type': 'application/json'}
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return Response.json({error: error.message}, {status: 500})
+        }
+    }
+}
