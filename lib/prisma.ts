71aee02256098267fd1415eda8b92480f4337a98
SalinChhun <salinchhun123079@gmail.com>
2025-05-27 09:58:56 +0700

    fix eslin 8

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,41 @@
 // @ts-nocheck
 import {PrismaClient} from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient(
+    {
+        log: process.env.NODE_ENV === 'production' ? [] : ['query', 'info', 'warn', 'error'],
     }
-    prisma = global.prisma;
+);
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
 }
 
-export default prisma;
+process.on('SIGINT', async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+})
+
+process.on('SIGTERM', async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+})
+
+process.on('beforeExit', async () => {
+    await prisma.$disconnect();
+})
+
+// let prisma: PrismaClient;
+//
+// if (process.env.NODE_ENV === 'production') {
+//     prisma = new PrismaClient();
+// } else {
+//     if (!global.prisma) {
+//         global.prisma = new PrismaClient();
+//     }
+//     prisma = global.prisma;
+// }
+//
+// export default prisma;
