786efd4029f038ce5aefa3a126389eec096071b4
SalinChhun <salinchhun123079@gmail.com>
2025-05-28 13:27:06 +0700

    fix update telegram

--- app/api/telegram/webhook/route.ts
+++ app/api/telegram/webhook/route.ts
@@ -5,7 +5,7 @@ import {TelegramUpdate} from "@/app/type/telegram";
 
 export async function POST(request: NextRequest) {
     try {
-        const update: TelegramUpdate = await request.json();
+        const update = await request.json();
         console.log('Received Telegram update:', update);
 
         //TODO: Check if this is a message update
