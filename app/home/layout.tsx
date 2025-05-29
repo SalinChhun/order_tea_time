bfd6b9ba032b73a74885bcbcc003d4a025c8382f
SalinChhun <salinchhun123079@mail.com>
2025-05-29 11:51:34 +0700

    add tailwind

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 import {PropsWithChildren} from "react";
+import "@/globals.css";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
